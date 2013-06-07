0a81461c6e9e88aa82b33f09bbee2069c1328947
SevenCloud <linyouqing7@sina.com>
2013-06-07 17:21:42 +0800

    add shell 20130607

--- libosnstor.h
+++ libosnstor.h
@@ -1,6 +1,8 @@
 #ifndef LIBOSNSTOR_H_
 #define LIBOSNSTOR_H_
-#include"list.h"
+#include "list.h"
+
+#define CHAR_LEN 64
 
 struct scsi_host {
 	struct list_head host_entry;
@@ -11,15 +13,15 @@ struct scsi_host {
 	/* host unique identifier */
 	unsigned int host_id;
 
-	/*device name, for example host1,host2 */
-	const char *devname;
+	/*host name, for example host1,host2 */
+	const char hostname[CHAR_LEN];
 
 	/* qla2xxx/iscsi_tcp/... */
-	const char *driver_type;
+	const char driver_type[CHAR_LEN];
 
 	unsigned int transport_type;
 
-	const char *port_name;
+	const char port_name;
 	uint32_t port_id;
 };
 
@@ -29,9 +31,9 @@ struct scsi_dev {
 	unsigned int host, channel, target, lun;
 	unsigned int major, minor;
 
-	const char *vendor;
-	const char *model;
-	const char *rev;
+	const char vendor[CHAR_LEN];
+	const char model[CHAR_LEN];
+	const char rev[CHAR_LEN];
 
 	char type;
 	char scsi_level;
